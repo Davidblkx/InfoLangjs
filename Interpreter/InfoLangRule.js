05e7258d83c31dad33f898b6eae5da6cdc11215e
david pires <davidblkx@gmail.com>
2015-05-07 00:01:16 +0100

    removed jquery dependency and code cleaning

--- Interpreter/InfoLangRule.js
+++ Interpreter/InfoLangRule.js
@@ -6,11 +6,17 @@ PrototypeRule = function(){
   return {
 
       Name: "RuleName",
+      Key: "",
+      Value: "",
 
       Regex: [],
 
       Parse: function(stringSource){
           return stringSource;
+      },
+
+      BuildElement : function(objData){
+          return document.createElement('div');
       }
 
   };
@@ -19,15 +25,14 @@ PrototypeRule = function(){
 InfoLangSimpleRule = function(name, startRule, endRule) {
 
     var rule = new PrototypeRule();
-
     rule.Name = name;
-
     rule.Regex.push(new RegExp(startRule + "(.*)" + endRule, "g"));
 
     rule.Parse = function(stringSource){
         var rez = [];
 
-        for (i = 0; i < this.Regex.length; i++) {
+        //Get items from source string
+        for (var i = 0; i < this.Regex.length; i++) {
             var matchResult = stringSource.match(this.Regex[i]);
 
             if(matchResult == undefined)
@@ -49,36 +54,80 @@ InfoLangSimpleRule = function(name, startRule, endRule) {
             }
         }
 
+        //If no item is parsed, an empty object is returned
         if(rez.length === 0)
             return (JSON.parse('{"' + rule.Name + '" : []}'));
 
+        //remove repeated items
         var unique = [];
+        for(var itmId = 0; itmId < rez.length; itmId++){
 
-        $.each(rez, function(i, el){
-            if($.inArray(el, unique) === -1) unique.push(el);
-        });
+            var item = rez[itmId];
+            var hasItem = false;
+            for(hasId = 0; hasId < unique.length; hasId++){
+                if(unique[hasId] === item){
+                    hasItem = true;
+                    break;
+                }
+            }
 
-        var result = '{ "' + rule.Name +'" : [';
+            if(!hasItem){
+                unique.push(item);
+            }
 
-        for(i=0; i<unique.length;i++){
-            result += '"' + unique[i] + '",';
         }
 
-        result  = result.substring(0, result.length-1) + ']}';
+        //Build Object
+        var result = '{ "' + rule.Name +'" : ' + JSON.stringify(unique) + '}';
         return JSON.parse(result);
+    };
+
+    rule.BuildElement = function(objData){
+
+        var rootDiv = document.createElement('div');
+        rootDiv.setAttribute("data-IL-Id", rule.Name);
+        rootDiv.className = "iL-View iL-ViewSR";
+
+        var titleH1 = document.createElement('H1');
+        titleH1.innerHTML = rule.Name;
+        titleH1.className = "iL-ViewItem iL-ViewItemSR iL-ViewItemSR-Title";
+
+        rootDiv.appendChild(titleH1);
+
+        var arr = objData[rule.Name];
+        for(var i=0; i < arr.length; i++){
+            var valueDiv = document.createElement('div');
+            valueDiv.setAttribute("data-IL-SR-ItemIndex", i);
+            valueDiv.className = "iL-ViewItem iL-ViewItemSR iL-ViewItemSR-ValueHost";
+
+            var valueInput = document.createElement('input');
+            valueInput.type = "text";
+            valueInput.setAttribute("data-IL-SR-ItemIndex", i);
+            valueInput.className = "iL-ViewItem iL-ViewItemSR iL-ViewItemSR-Value";
+            valueInput.value = arr[i];
+
+            valueDiv.appendChild(valueInput);
+            rootDiv.appendChild(valueDiv);
+        }
+
+        return rootDiv;
     };
 
     return rule;
 };
 
-InfoLangMultiRule = function(name, startRule, separator, endRule){
+InfoLangMultiRule = function(name, key, value,  startRule, separator, endRule){
+
     var rule = new PrototypeRule();
     rule.Name = name;
+    rule.Key = key;
+    rule.Value = value;
     rule.Regex.push(new RegExp(startRule + "(.*)" + separator + "([0-9])+" + endRule, "g"));
 
     rule.Parse = function(stringSource){
-        var rez = [];
 
+        //Parse items from source string
+        var rez = [];
         for (i = 0; i < this.Regex.length; i++) {
             var matchResult = stringSource.match(this.Regex[i]);
 
@@ -99,15 +148,30 @@ InfoLangMultiRule = function(name, startRule, separator, endRule){
             }
         }
 
+        //If no item is parsed, an empty object is returned
         if(rez.length == 0)
-            return rez;
+            return JSON.parse('{"' + rule.Name + '" : ' + JSON.stringify(rez) + '}');
 
+        //remove repeated items
         var unique = [];
+        for(var itmId = 0; itmId < rez.length; itmId++){
 
-        $.each(rez, function(i, el){
-            if($.inArray(el, unique) === -1) unique.push(el);
-        });
+            var item = rez[itmId];
+            var hasItem = false;
+            for(hasId = 0; hasId < unique.length; hasId++){
+                if(unique[hasId] === item){
+                    hasItem = true;
+                    break;
+                }
+            }
 
+            if(!hasItem){
+                unique.push(item);
+            }
+
+        }
+
+        //Build object from items
         rez = [];
         for(i=0; i<unique.length;i++){
 
@@ -115,11 +179,61 @@ InfoLangMultiRule = function(name, startRule, separator, endRule){
             var name = str.substring(startRule.length, str.indexOf(';'));
             var nums = str.substring(str.indexOf(';')+1).split(';');
 
-            var obj = JSON.parse('{"'+name+'" : ' + JSON.stringify(nums) + '}');
+            var obj = JSON.parse('{ "'+ key + '" : "'+name+'", "' + value + '" : ' + JSON.stringify(nums) + '}');
             rez.push(obj);
         }
 
         return JSON.parse('{"' + rule.Name + '" : ' + JSON.stringify(rez) + '}');
+    };
+
+    rule.BuildElement = function(objData){
+
+        var rootDiv = document.createElement('div');
+        rootDiv.setAttribute("data-IL-Id", rule.Name);
+        rootDiv.className = "iL-View iL-ViewMR";
+
+        var titleH1 = document.createElement('H1');
+        titleH1.innerHTML = rule.Name;
+        titleH1.className = "iL-ViewItem iL-ViewItemMR iL-ViewItemMR-Title";
+
+        rootDiv.appendChild(titleH1);
+
+        var arr = objData[rule.Name];
+        for(var i=0; i < arr.length; i++){
+
+            var Key = arr[i][rule.Key];
+            var Values = arr[i][rule.Value];
+
+            var keyDiv = document.createElement('div');
+            keyDiv.setAttribute("data-IL-MR-Key", key);
+            keyDiv.className = "iL-ViewItem iL-ViewItemMR iL-ViewItemMR-Host";
+
+            var titleH3 = document.createElement('H3');
+            titleH3.innerHTML = Key;
+            titleH3.className = "iL-ViewItem iL-ViewItemMR iL-ViewItemMR-SubTitle";
+
+            keyDiv.appendChild(titleH3);
+
+            for(var valId=0; valId < Values.length; valId++){
+                var valueDiv = document.createElement('div');
+                valueDiv.setAttribute("data-IL-MR-ItemIndex", valId);
+                valueDiv.className = "iL-ViewItem iL-ViewItemMR iL-ViewItemMR-ValueHost";
+
+                var valueInput = document.createElement('input');
+                valueInput.type = "text";
+                valueInput.setAttribute("data-IL-MR-ItemIndex", valId);
+                valueInput.className = "iL-ViewItem iL-ViewItemMR iL-ViewItemMR-Value";
+                valueInput.value = Values[valId];
+
+                valueDiv.appendChild(valueInput);
+                keyDiv.appendChild(valueDiv);
+            }
+
+
+            rootDiv.appendChild(keyDiv);
+        }
+
+        return rootDiv;
     };
 
     return rule;
--- Interpreter/infoLangInterop.js
+++ Interpreter/infoLangInterop.js
@@ -12,6 +12,47 @@ var InfoLangInterop = {
         for(v=0;v<this.Rules.length;v++){
             arr.push(this.Rules[v].Parse(source))
         }
-        return arr;
+
+        var ptp = '{';
+
+        for(v=0;v<arr.length;v++){
+
+            var txt = JSON.stringify(arr[v]);
+            txt = txt.substring(1, txt.length-1);
+
+            if(v>0)
+                txt = ',' + txt;
+
+            ptp += txt;
+        }
+
+        ptp += '}';
+
+        return JSON.parse(ptp);
+    },
+
+    SetWatchOn : function(elemId, viewElemId , onInput){
+
+        var elem = document.getElementById(elemId);
+        var sourceRules = this.Rules;
+        elem.addEventListener("input", function(){
+
+            var DATA = InfoLangInterop.Parse(elem.value);
+
+            if(viewElemId != undefined) {
+                var view = document.getElementById(viewElemId);
+                while (view.hasChildNodes()) {
+                    view.removeChild(view.lastChild);
+                }
+
+                for (var ruleId = 0; ruleId < sourceRules.length; ruleId++) {
+                    view.appendChild(sourceRules[ruleId].BuildElement(DATA));
+                }
+            }
+
+            if(onInput != undefined)
+                onInput(DATA);
+
+        });
     }
 };
